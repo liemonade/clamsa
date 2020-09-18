4de9d35f0bd90166809e1144e3f37240ed41bf0a
Mario Stanke <mario.stanke@gmail.com>
2020-09-18 13:29:37 +0200

    added keywork remove_stop_rows to argument

--- utilities/model_evaluation.py
+++ utilities/model_evaluation.py
@@ -256,7 +256,7 @@ def predict_on_fasta_files(trial_ids, # OrderedDict of model ids with keys like 
         for f in fasta_paths:
 
             # filter fasta files that have no valid reference clade
-            cid, sl, S = msa_converter.parse_fasta_file(f, clades, trans_dict=trans_dict, remove_stop_rows)
+            cid, sl, S = msa_converter.parse_fasta_file(f, clades, trans_dict=trans_dict, remove_stop_rows=remove_stop_rows)
             if cid == -1:
                 path_ids_without_reference_clade.add(f)
                 continue
